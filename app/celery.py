b8999f2224ac42f73e23498719444ff40d616e5b
Arrow Dan <danielkamau231@gmail.com>
2025-04-17 16:51:01 +0300

    chore: update .env variables and perform code cleanup

--- app/celery.py
+++ app/celery.py
@@ -5,6 +5,6 @@ from django.conf import settings
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-app = Celery('app')
+app = Celery('app', broker="redis://localhost:6379/0")
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
