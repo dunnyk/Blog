aebdca5690b57e254190af205a4c666fe1bb6a25
Arrow Dan <danielkamau231@gmail.com>
2025-04-22 23:15:17 +0300

    test: Add unit test for User model

---
+++ app/api/authentication/tests/conftest.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import django
+
+# Add project root to PYTHONPATH
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../.."))
+)
+
+# Set Django settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+
+# Initialize Django
+django.setup()
